4df39c831624f662e6f12e32b5009d126e31f9f3
depattaf-spec <depatta.f@gmail.com>
2026-03-23 23:14:28 +0100

    Create Firebase config endpoint
    Add Firebase configuration endpoint serving from environment variables.

---
+++ api/firebase-config.js
@@ -0,0 +1,12 @@
+module.exports = (req, res) => {
+  // Serve Firebase config from environment variables (never from source code)
+  res.setHeader('Cache-Control', 'no-store');
+  res.json({
+    apiKey:            process.env.FIREBASE_API_KEY,
+    authDomain:        process.env.FIREBASE_AUTH_DOMAIN,
+    projectId:         process.env.FIREBASE_PROJECT_ID,
+    storageBucket:     process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId:             process.env.FIREBASE_APP_ID
+  });
+};
